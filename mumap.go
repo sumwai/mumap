0f028da0563f1d1011e6a29bbbc64d63f2c9986a
sumwai <sumwai@qq.com>
2023-02-12 11:25:51 +0800

    mumap: add Each function for each all item in map

--- mumap.go
+++ mumap.go
@@ -2,11 +2,16 @@ package mumap
 
 import "sync"
 
+// mumap struct
 type Mumap[K comparable, V interface{}] struct {
 	mu  sync.Mutex
 	Map map[K]V
 }
 
+// EachFunc if return true, that will be break, otherwise, continue
+type EachFunc[K comparable, V interface{}] func(K, V) bool
+
+// Set like `map[k] = v`, but return mumap
 func (m *Mumap[K, V]) Set(k K, v V) *Mumap[K, V] {
 	m.mu.Lock()
 	defer m.mu.Unlock()
@@ -14,6 +19,7 @@ func (m *Mumap[K, V]) Set(k K, v V) *Mumap[K, V] {
 	return m
 }
 
+// Get returns value and ok, like `v, ok := map[k]`
 func (m *Mumap[K, V]) Get(k K) (v V, ok bool) {
 	m.mu.Lock()
 	defer m.mu.Unlock()
@@ -21,6 +27,21 @@ func (m *Mumap[K, V]) Get(k K) (v V, ok bool) {
 	return
 }
 
+// Each range the mumap, call that item uses EachFunc.
+// that for loop will break if EachFunc returns `true`
+// otherwise will be continue
+func (m *Mumap[K, V]) Each(do EachFunc[K, V]) {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+
+	for k, v := range m.Map {
+		if do(k, v) {
+			break
+		}
+	}
+}
+
+// New Create a new mumap
 func New[K comparable, V interface{}]() (m *Mumap[K, V]) {
 	m = new(Mumap[K, V])
 	m.Map = map[K]V{}
--- mumap_test.go
+++ mumap_test.go
@@ -1,16 +1,17 @@
 package mumap
 
 import (
+	"math/rand"
 	"strconv"
 	"sync"
 	"testing"
 )
 
-func TestMumap(t *testing.T) {
+func TestMumapGetSet(t *testing.T) {
 	type Point struct {
 		X, Y int
 	}
-	var c int = 1e7 + 1
+	var c int = 1e4 + 1
 
 	m := New[string, *Point]()
 	var i int
@@ -26,5 +27,44 @@ func TestMumap(t *testing.T) {
 
 	if d, ok := m.Get(strconv.Itoa(c)); !ok || d.X != c || d.Y != c {
 		t.Fatal("Error", d, ok)
+	}
+}
+
+func TestMumapEach(t *testing.T) {
+	var c int = 1e4
+	var want int = 0
+	var got int = 0
+	var num int = 100
+
+	m := New[int, int]()
+
+	mutex := sync.Mutex{}
+	wg := sync.WaitGroup{}
+	wg.Add(c + 1)
+	// Set Value
+	for i := 0; i <= c; i++ {
+		go func(i int) {
+			n := rand.Intn(1000)
+			// want++, if that random number is `num`
+			if n == num {
+				mutex.Lock()
+				want++
+				mutex.Unlock()
+			}
+			m.Set(i, n)
+			wg.Done()
+		}(i)
+	}
+	wg.Wait()
+
+	m.Each(func(_, v int) bool {
+		if v == num {
+			got++
+		}
+		return false
+	})
+
+	if got != want {
+		t.Fatalf("Each error, Want: %d, but Got: %d", want, got)
 	}
 }
